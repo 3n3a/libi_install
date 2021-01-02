3b338d9092dc5037eef827a9b4d2f152e58b37ca
3n3a <46775561+3n3a@users.noreply.github.com>
2021-01-02 01:17:14 +0100

    Update install_itunes_untested.bat

--- .stuff/install_itunes_untested.bat
+++ .stuff/install_itunes_untested.bat
@@ -1,8 +1,8 @@
 @echo off
 
 curl https://www.apple.com/itunes/download/win64 -L -o itunes64.exe
-start /wait /i itunes64.exe /qn /norestart
-OR 
+rem start /wait /i itunes64.exe /qn /norestart
+rem OR 
 start itunes64.exe
 del itunes64.exe
 
